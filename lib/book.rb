f845e755b1962977c036ddceb5b18dfbc1e8a008
Maverick Adolfo <merkonical@gmail.com>
2019-09-01 05:23:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,5 +23,30 @@ class Book
   def page_count
     @page_count
   end 
+  
+  def genre=(genre)
+    @genre = genre
+  end 
+  
+  def genre 
+    @genre
+  end 
 
 end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
